957f50d96c5977e9ae100f72968ac33e2ce96d95
mkarimi53 <maryam.karimi7653@gmail.com>
2018-10-12 00:35:09 +0330

    Fetch_Image print result and time

--- Encoder.c
+++ Encoder.c
@@ -1,3 +1,4 @@
+#include <time.h>
 #include "Coding.h"
 
 // image data type
@@ -90,6 +91,13 @@ void Fetch_Image(char *Filename, image *Source_Image)
    double *Pixel_Data;
    FILE *Source_File;
 
+   char* resultFilename = "Fetch_Image.txt";
+   FILE* result;
+
+   clock_t start, end;
+   double cpu_time_used;
+
+   start = clock();
    // open the file
    if ((Source_File = fopen(Filename, "rb")) == NULL) {
       printf("Problem opening source image %s\n", Filename);
@@ -116,6 +124,30 @@ void Fetch_Image(char *Filename, image *Source_Image)
    Source_Image->Rows = Rows;
    Source_Image->Columns = Columns;
    Source_Image->Pixel_Data = Pixel_Data;
+
+   end = clock();
+   cpu_time_used = ((double) (end - start)) /CLOCKS_PER_SEC;
+
+   // print result to Fetch_Image
+   printf("result\n");
+   if ((result = fopen(resultFilename, "w")) == NULL) {
+      printf("Problem creating %s.\n", resultFilename);
+      exit(0);
+   }
+   fprintf(result, "%d\n", Rows);
+   fprintf(result, "%d\n", Columns);
+   printf("%d\n", Rows);
+   printf("%d\n", Columns);
+   for(int i = 0; i < Rows; i++){
+     for(int j = 0; j < Columns; j++){
+       for(int k = 0; k < 3; k++){
+         //printf("%d %d %d\n", i, j, k);
+         fprintf(result, "%f\n", Pixel_Data[(i * Columns * 3) + (j * 3) + k]);
+       }
+     }
+   }
+   fprintf(result, "%f\n", cpu_time_used);
+   fclose(result);
 }
 
 void colourSpaceConversion(image *Source_Image, double *Source_Data){
